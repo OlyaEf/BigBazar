cf34458128157cfeaf97cb6e9c682a095566ca62
OlyaEf <efimovskikh@gmail.com>
2023-11-15 09:30:09 +0100

    [FEATURE]  fix-project #22

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
+from httpx import AsyncClient
 from tortoise import Tortoise
 
 from bb.core.config import MODELS, POSTGRES_PASSWORD, \
     POSTGRES_HOST, POSTGRES_PORT
+from bb.main import app
 
 TEST_DB_URL = f'postgres://postgres:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/db_test_bb'
 
@@ -30,3 +32,42 @@ def test_db(event_loop):
     event_loop.run_until_complete(init())
     yield
     event_loop.run_until_complete(fini())
+
+
+@pytest.fixture
+async def register_and_authenticate_user():
+    async with AsyncClient(app=app, base_url="http://testserver") as client:
+        # Регистрация пользователя
+        await client.post("/users/register", json={
+            "name": "Test User",
+            "email": "test@example.com",
+            "phone": "+71234567890",
+            "password": "Password123!",
+            "confirm_password": "Password123!"
+        })
+
+        # Аутентификация пользователя
+        login_response = await client.post("/users/login", json={
+            "email": "test@example.com",
+            "password": "Password123!"
+        })
+        access_token = login_response.json()["access_token"]
+        headers = {"Authorization": f"Bearer {access_token}"}
+        return headers
+
+
+# conftest.py
+
+@pytest.fixture
+async def registered_user_by_id():
+    async with AsyncClient(app=app, base_url="http://testserver") as client:
+        # Регистрация пользователя
+        response = await client.post("/users/register", json={
+            "name": "Test User",
+            "email": "test@example.com",
+            "phone": "+71234567890",
+            "password": "Password123!",
+            "confirm_password": "Password123!"
+        })
+        user_id = response.json()["id"]  # Получение ID пользователя
+        return user_id
--- tests/users_test.py
+++ tests/users_test.py
@@ -2,6 +2,7 @@ import pytest
 from httpx import AsyncClient
 
 from bb.main import app
+from bb.users.models import User
 
 
 @pytest.mark.asyncio
@@ -10,23 +11,36 @@ async def test_user_register(test_db):
         response = await client.post(
             "/users/register",
             json={
-                "name": "Test_Register",
-                "email": "test.reg@example.com",
-                "phone": "+71234567897",
-                "password": "Reg!Password123!",
-                "confirm_password": "Reg!Password123!"
+                "name": "Test3_Register",
+                "email": "test3.reg@example.com",
+                "phone": "+71234567893",
+                "password": "Reg!Password123!3",
+                "confirm_password": "Reg!Password123!3"
                }
             )
         print(response)
         assert response.status_code == 200
-        assert response.json()["name"] == "Test_Register"
-        assert response.json()["email"] == "test.reg@example.com"
-        assert response.json()["phone"] == "+71234567897"
+        assert response.json()["name"] == "Test3_Register"
+        assert response.json()["email"] == "test3.reg@example.com"
+        assert response.json()["phone"] == "+71234567893"
+
+        # Очистка данных в конце теста
+        await User.all().delete()
 
 
 @pytest.mark.asyncio
 async def test_user_login(test_db):
     async with AsyncClient(app=app, base_url="http://testserver") as client:
+        # Сначала регистрируем пользователя
+        await client.post("/users/register", json={
+            "name": "Test User",
+            "email": "test.reg@example.com",
+            "phone": "+71234567890",
+            "password": "Reg!Password123!",
+            "confirm_password": "Reg!Password123!"
+        })
+
+        # Затем пытаемся выполнить вход
         response = await client.post(
             "/users/login",
             json={
@@ -36,22 +50,23 @@ async def test_user_login(test_db):
         )
         print(response)
         assert response.status_code == 200
-        if response.status_code != 200:
-            print(response.text)
         assert "access_token" in response.json()
 
 
 @pytest.mark.asyncio
-async def test_get_all_users(test_db):
+async def test_get_all_users(test_db, register_and_authenticate_user):
+    headers = await register_and_authenticate_user
     async with AsyncClient(app=app, base_url="http://testserver") as client:
-        response = await client.get("/users")
+
+        response = await client.get("users/list", headers=headers)
         assert response.status_code == 200
         assert isinstance(response.json(), list)
 
 
 @pytest.mark.asyncio
-async def test_get_user_by_id(test_db):
-    user_id = 1  # предполагаем, что такой пользователь существует
+async def test_get_user_by_id(test_db, registered_user_by_id):
+    user_id = await registered_user_by_id  # Получаем ID зарегистрированного пользователя
+
     async with AsyncClient(app=app, base_url="http://testserver") as client:
         response = await client.get(f"/users/{user_id}")
         assert response.status_code == 200
@@ -75,6 +90,4 @@ async def test_delete_user(test_db):
         response = await client.delete(f"/users/{user_id}")
         assert response.status_code == 200
         assert response.json()["message"] == "User deleted successfully"
-
-
 
